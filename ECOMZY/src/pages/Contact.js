bb2974fa42bcdb3343092374452b391d20354a1d
Sangam Rai <138009367+sangamrai001@users.noreply.github.com>
2024-07-23 05:19:42 +0530

    Add files via upload

---
+++ ECOMZY/src/pages/Contact.js
@@ -0,0 +1,25 @@
+export default function Contact(){
+    return (
+        <div className="w-full  flex  flex-col ">
+            <div className=" item-center m-auto w-2/3 ">
+
+                <p className=" mt-5 font-semibold text-gray-700">
+               Thank you for visiting our e-commerce platform.
+                We value your feedback and are here to assist you with any questions or concerns.
+               </p>
+              
+            
+            </div>
+            <div className="w-full bg-blue flex justify-center mt-5">
+                <div className="m-auto items-center" >
+                        Email: sangam@gmail.com<br/>
+                        Phone: +913-456-7890<br/>
+                        Address: 1234 Y-street, Delhi, India
+                </div>
+            </div>
+            
+
+
+        </div>
+    )
+}
