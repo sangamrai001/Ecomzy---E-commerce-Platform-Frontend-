5f9001025279e4543937dfd720f9c80b81c90198
Sangam Rai <138009367+sangamrai001@users.noreply.github.com>
2024-11-04 19:50:13 +0530

    Delete ECOMZY directory

--- ECOMZY/src/redux/store.jsx
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import NewSlices from './slices/NewSlices';
-
-export const store = configureStore({
-  reducer: {
-    cart:NewSlices,
-  },
-});
- 
